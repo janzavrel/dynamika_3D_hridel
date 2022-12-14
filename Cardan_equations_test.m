d6178544dea369a4e54fdf022f88591378493724
Jan Zavřel <Jan.Zavrel@fs.cvut.cz>
2022-12-14 10:56:20 +0100

    Image folder created

---
+++ Cardan_equations_test.m
@@ -0,0 +1,18 @@
+%%Test of integration
+
+%% movement definition
+%initial angular position
+fi1x0=2;
+fi1y0=3;
+fi1z0=4;
+
+%initial angular velocity
+fi1xt0=0.2;
+fi1yt0=0.3;
+fi1zt0=0.4;
+
+%angular acceleration
+fi1xtt=1.2;
+fi1ytt=2.3;
+fi1ztt=3.4;
+
